61e3ff3f23409e9b5fff007439da50833e495599
amr osama <amr.osama1995@gmail.com>
2017-04-19 16:36:16 +0200

    First commit

--- Ab/ViewController.swift
+++ Ab/ViewController.swift
@@ -9,9 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var prevbutt: UIButton!
+    var stringo = String()
+    var arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]
+    
+    @IBAction func click(_ sender: UIButton) {
+        if(stringo.isEmpty){
+            prevbutt = sender
+           let a = sender.tag
+            stringo = String(arr[a])
+           sender.setTitle(stringo, for: .normal)
+            
+            
+        }else{
+            if(sender != prevbutt){
+            let bit = sender
+            sender.setTitle(String(arr[bit.tag]), for: .normal)
+            let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+            DispatchQueue.main.asyncAfter(deadline: when) {
+               
+            
+            if(self.arr[bit.tag] == self.arr[self.prevbutt.tag]){
+                bit.isHidden = true
+                
+                self.prevbutt.isHidden = true
+                
+                
+                
+            }else{
+                bit.setTitle("Button", for: .normal)
+                self.prevbutt.setTitle("Button", for: .normal)
+                
+                
+                }
+                self.stringo = String()}}
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        for i in (1...15).reversed(){
+            let inii = i + 1
+          let index =  Int(arc4random_uniform(UInt32(inii)))
+            let a = arr[index];
+            arr[index] = arr[i];
+            arr[i] = a;
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
